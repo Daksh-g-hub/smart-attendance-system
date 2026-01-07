bc685f9d7126794a575ca796330f3f04390f1800
DAKSH CHHIMPA <dakshchhimpa@gmail.com>
2026-01-07 18:43:26 +0530

    Create face_register.py

---
+++ face_register.py
@@ -0,0 +1,10 @@
+import cv2
+
+def capture_faces(student_id, name):
+    # TODO: capture images
+    pass
+
+if __name__ == "__main__":
+    sid = input("Enter Student ID: ")
+    name = input("Enter Name: ")
+    capture_faces(sid, name)
